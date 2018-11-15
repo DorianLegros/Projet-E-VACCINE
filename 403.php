403106c04fc212352349e62bee9859cebeb424c2
Dorian Legros <d.ratha76@hotmail.com>
2018-11-15 16:51:17 +0100

    v3.28

--- 403.php
+++ 403.php
@@ -6,6 +6,8 @@
 
 <?php include('inc/header.php'); ?>
 
+<div class="wrap">
 <p class="er404"><span class="maj">Erreur 403</span> vous n'avez pas les droits nécessaires pour accéder à cette page !</p>
+</div>
 
 <?php include('inc/footer.php'); ?>
